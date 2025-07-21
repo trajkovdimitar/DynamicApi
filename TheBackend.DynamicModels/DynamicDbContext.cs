53e3556f6f21bbc45d992ba459a341281d12bf3a
Trajkov Dimitar <45421293+trajkovdimitar@users.noreply.github.com>
2025-07-22 00:34:53 +0200

    Remove GitHub Actions workflow

--- TheBackend.DynamicModels/DynamicDbContext.cs
+++ TheBackend.DynamicModels/DynamicDbContext.cs
@@ -4,10 +4,10 @@ namespace TheBackend.DynamicModels
     public class DynamicDbContext : DbContext
     {
         public DynamicDbContext(DbContextOptions<DynamicDbContext> options) : base(options) { }
-      
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-       
+
         }
     }
 }
