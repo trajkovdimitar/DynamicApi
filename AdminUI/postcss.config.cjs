e00cc419908314c48209ea1ec5a4ed3286d4d968
Trajkov Dimitar <45421293+trajkovdimitar@users.noreply.github.com>
2025-07-22 10:09:50 +0200

    Use standard tailwind postcss plugin config

--- AdminUI/postcss.config.cjs
+++ AdminUI/postcss.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
-  plugins: [
-    require('@tailwindcss/postcss')(), // <- updated!
-    require('autoprefixer')
-  ]
-}
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+};
