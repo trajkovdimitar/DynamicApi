4c85fe0a4b83606d3ad88915f5f152e5439889fc
Trajkov Dimitar <45421293+trajkovdimitar@users.noreply.github.com>
2025-07-22 02:20:03 +0200

    Add dynamic collection UI components

--- TheBackend.Admin/Program.cs
+++ TheBackend.Admin/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TheBackend.Admin;
 using TheBackend.Admin.Shared;
+using TheBackend.Admin.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7020/") });
 builder.Services.AddScoped<ApiClient>();
+builder.Services.AddScoped<CollectionService>();
 
 await builder.Build().RunAsync();
